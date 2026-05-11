3ae5380344b6f1515e7c15bb8f186db6c0696a3f
zrangonazaire <61186442+zrangonazaire@users.noreply.github.com>
2026-05-11 16:14:04 +0000

    Envoi des MAJ

--- calipso-backend/src/main/java/com/calipso/config/SecurityConfig.java
+++ calipso-backend/src/main/java/com/calipso/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(false);
